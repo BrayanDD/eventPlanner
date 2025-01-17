f4906b974ea814bbe1fc06c49ac5678053aa528c
BrayanDelgado <isosbrayan@gmail.com>
2025-01-17 14:40:54 -0500

    deploy

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -11,33 +11,33 @@ class InvitationController extends Controller
     public function index()
     {
 
-        $invitations = Invitation::where('user_id',auth()->id())->where('state_id',State::PENDING)->with('user', 'event')->paginate(20);
-       
-       
-        
+        $invitations = Invitation::where('user_id',auth()->id())->where('state_id',State::pendiente)->with('user', 'event')->paginate(20);
+
+
+
         return view('invitations.index', compact('invitations'));
     }
     public function store(Request $request)
     {
-     
+
        $this->authorize('create',Invitation::class);
 
         $request->validate([
             'user_ids' => 'required|array',
-            'user_id.*' => 'exists:users,id', 
+            'user_id.*' => 'exists:users,id',
             'event_id' => 'required|exists:events,id',
         ]);
-    
+
         $event_id = $request->event_id;
-    
+
         foreach ($request->user_ids as $user_id) {
-           
+
             $existingInvitation = Invitation::where('user_id', $user_id)
                                             ->where('event_id', $event_id)
                                             ->first();
-    
+
             if (!$existingInvitation) {
-           
+
                 Invitation::create([
                     'user_id' => $user_id,
                     'event_id' => $event_id,
@@ -45,10 +45,10 @@ class InvitationController extends Controller
                 ]);
             }
         }
-    
+
         return back()->with(['success' => true, 'message' => 'Invitaciones enviadas correctamente.']);
     }
-    
+
     public function update(Request $request, Invitation $invitation)
     {
         $this->authorize('update',$invitation);
@@ -60,10 +60,10 @@ class InvitationController extends Controller
 
         $invitation->update([
             'state_id' => $request->state_id,
-          
+
         ]);
 
         return redirect()->route('invitations.index');
     }
-    
+
 }
--- app/Models/State.php
+++ app/Models/State.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    const PENDING = 1;
-    const ACCEPTED = 2;
-    const REJECTED = 3;
+    const pendiente = 1;
+    const aceptado = 2;
+    const rechazado = 3;
     protected $fillable = [
         'name',
-       
+
 
     ];
     public function invitations()
