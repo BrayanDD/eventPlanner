0005a9e81ef5effe8f04508c82f3bce96b733c0a
BrayanDelgado <isosbrayan@gmail.com>
2025-01-17 15:05:53 -0500

    db

--- database/migrations/2024_10_16_163327_create_states_table.php
+++ database/migrations/2024_10_16_163327_create_states_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB; // Importar DB para ejecutar inserciones.
 
 return new class extends Migration
 {
@@ -16,6 +17,13 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->timestamps();
         });
+
+        // Insertar valores predeterminados
+        DB::table('states')->insert([
+            ['id' => 1, 'name' => 'pendiente', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'name' => 'aceptado', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'name' => 'rechazado', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
